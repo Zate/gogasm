1970a64bee97b408d12753e6f8f96ff9e5659f6c
Zate <zate75@gmail.com>
2019-01-31 19:21:34 -0600

    lets just let echo do wek, use traefik for proxy.

--- cmd/web.go
+++ cmd/web.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"net/http"
-	"net/url"
 
 	"github.com/labstack/echo"
 	"github.com/labstack/echo/middleware"
@@ -14,16 +13,12 @@ import (
 // Should display a error page in browser if those are not present, explaining
 // how to create those and to restart the server.
 
-// Ability to crawl Live servers and create a view on that
-// Live container stats via glances.  Need to reverse proxy to each container
-// for that.
+// Look at using vue.js to make it all pretty
 
 func initWeb(p string) {
 	e := echo.New()
-	e.Debug = true
-	// e.Static("/static", "static")
+	e.Static("/static", "static")
 	e.File("/favicon.ico", "favicon.ico")
-	// e.File("/common.css", "common.css")
 	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 	e.HideBanner = true
@@ -32,38 +27,8 @@ func initWeb(p string) {
 		AllowMethods: []string{echo.GET, echo.PUT, echo.POST, echo.DELETE},
 	}))
 
-	// a1url, err := url.Parse("http://atlas.zate.systems")
-	// if err != nil {
-	// 	e.Logger.Fatal(err)
-	// }
-	// a1target := []*middleware.ProxyTarget{
-	// 	{
-	// 		Name: "A1",
-	// 		URL:  a1url,
-	// 	},
-	// }
-
-	// rrb := middleware.NewRoundRobinBalancer(a1target)
-
-	// g := e.Group("/a1")
-	// g.Use(middleware.ProxyWithConfig(rrb))
-
-	a1url, err := url.Parse("http://atlas.zate.systems:80")
-	if err != nil {
-		e.Logger.Fatal(err)
-	}
-
-	targets := []*middleware.ProxyTarget{
-		{
-			URL: a1url,
-		},
-	}
 	e.GET("/", hello)
 	e.GET("/live", liveHandler)
-
-	g := e.Group("/a1")
-
-	g.Use(middleware.Proxy(middleware.NewRoundRobinBalancer(targets)))
 
 	e.Logger.Fatal(e.Start(":" + p))
 
